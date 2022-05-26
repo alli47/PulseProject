9ca61f1ce330ab02016106b309e72f17d9f9a9b8
Ali <allipazylov@gmail.com>
2022-05-26 21:15:17 +0500

    modal windows and footer were made up, an interactive map was added

--- src/js/script.js
+++ src/js/script.js
@@ -24,17 +24,38 @@ $(document).ready(function(){
 
 	function toggleSlide(item) {
 		$(item).each(function(i) {
-				$(this).on('click', function(e) {
-						e.preventDefault();
-						$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-						$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-				})
+			$(this).on('click', function(e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+			})
 		});
-};
+	};
 
-toggleSlide('.catalog-item__link');
-toggleSlide('.catalog-item__back');
+	toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__back');
+
+
+///Modal ept
+	$('[data-modal=consultation]').on('click', function() {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+	$('.modal__close').on('click', function(){
+		$('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+	});
+	
+	$('.button_mini').each(function(i){
+		$(this).on('click', function(){
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		})
+	});
 });
+
+
+
+
+
 /*const slider = tns({
 	container: '.carousel__inner',
 	items: 1,
